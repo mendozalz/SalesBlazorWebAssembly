a1ec5257c60cb8505216eb99de1b7f054a5ead1f
mendozalz <mendoza124302@gmail.com>
2024-10-20 23:08:02 -0500

    completando la API Controller

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -5,29 +5,71 @@ using Sales.Shared.Entities;
 
 namespace Sales.API.Controllers
 {
-	[ApiController]
-	[Route("/api/countries")]
-	public class CountriesController : ControllerBase
-	{
-		private readonly DataContext _context;
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
 
-		public CountriesController(DataContext context)
+        public CountriesController(DataContext context)
         {
-			_context = context;
-		}
+            _context = context;
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> GetAsync() 
-		{
-			return Ok(await _context.Countries.ToListAsync());
-		}
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
 
-		[HttpPost]
-		public async Task<ActionResult> PostAsync(Country country)
-		{
-			_context.Add(country);
-			await _context.SaveChangesAsync();
-			return Ok(country);
-		}
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }  
+        
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Remove(country);
+                await _context.SaveChangesAsync();
+                return NoContent();
+
+            }
+
+        }
     }
 }
