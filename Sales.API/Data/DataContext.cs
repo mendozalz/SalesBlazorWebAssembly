fa90cc9febbbf1e20d438a03870bf599b3bb4909
mendozalz <mendoza124302@gmail.com>
2024-10-29 22:39:33 -0500

    Agregando la Entidad de Categorias a la base de datos

--- Sales.API/Data/DataContext.cs
+++ Sales.API/Data/DataContext.cs
@@ -3,18 +3,29 @@ using Sales.Shared.Entities;
 
 namespace Sales.API.Data
 {
-	public class DataContext : DbContext
-	{
-        public DataContext(DbContextOptions<DataContext> options) : base(options) 
-        {    
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
         }
 
+        public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			base.OnModelCreating(modelBuilder);
-			modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
-		}
-	}
+        public DbSet<Category> Categories { get; set; }
+
+        //public DbSet<ProdCategory> ProdCategories { get; set; }
+
+        //public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+            modelBuilder.Entity<State>().HasIndex("CountryId", "Name").IsUnique();
+            modelBuilder.Entity<City>().HasIndex("StateId", "Name").IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(x =>x.Name).IsUnique();
+        }
+    }
 }
---
+++ Sales.Shared/Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Ciudad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public string Name { get; set; } = null!;
+        public State? State { get; set; }
+    }
+}
